e77808c24f74336a32acbd27f74232f0daf1a3d6
Osaremhen Ukpebor <osas.solo@outlook.com>
2024-08-17 21:33:55 +0100

    Added dark theme

--- weather_app/lib/Screens/WeatherScreen.dart
+++ weather_app/lib/Screens/WeatherScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../Constants/strings.dart';
+
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
 
@@ -14,10 +16,13 @@ class _WeatherScreenState extends State<WeatherScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.all(12.0),
         child: ListView(
           children: [
             SearchBar(
               controller: _cityController,
+              hintText: Strings.enterCityName,
+              leading: const Icon(Icons.search),
             ),
           ],
         ),
