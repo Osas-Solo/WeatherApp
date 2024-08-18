6211a110a7507c9ccc292de5c50e5aba17d2001c
Osaremhen Ukpebor <osas.solo@outlook.com>
2024-08-19 00:41:59 +0100

    Added models for daily weather

--- weather_app/lib/Constants/strings.dart
+++ weather_app/lib/Constants/strings.dart
@@ -1,4 +1,5 @@
 class Strings {
+  static final retrievingWeatherData = 'Retrieving weather data...';
   static final enterCityName = 'Enter city name';
   static final cityNotFound = 'City not found';
   static final sorryAnErrorOccurred = 'Sorry, an error occurred';
--- weather_app/lib/Screens/WeatherScreen.dart
+++ weather_app/lib/Screens/WeatherScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:weather_app/Models/CurrentCityWeatherData.dart';
+import 'package:weather_app/Models/DailyWeatherData.dart';
 
 import '../Constants/strings.dart';
 
@@ -17,28 +18,50 @@ class _WeatherScreenState extends State<WeatherScreen> {
   final String OPEN_WEATHER_API_KEY = dotenv.env['OPEN_WEATHER_API_KEY']!;
   final String OPEN_WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
   CurrentCityWeatherData? _currentCityWeatherData = null;
+  DailyWeatherData? _dailyWeatherData = null;
+  List<ThreeHourWeatherData> fiveDayWeatherDataList = [];
 
   Future<void> _searchWeather() async {
+    ScaffoldMessenger.of(context).showSnackBar(new SnackBar(content: Text(Strings.retrievingWeatherData)));
+
     final String cityName = _cityController.text;
 
     var cityCurrentWeatherURL = Uri.parse('$OPEN_WEATHER_API_URL/weather?q=$cityName&appID=$OPEN_WEATHER_API_KEY');
     final cityCurrentWeatherResponse = await http.get(cityCurrentWeatherURL);
 
-    print('Response status:');
-    print(cityCurrentWeatherResponse.body);
-    
     if (cityCurrentWeatherResponse.statusCode == 200) {
-      print('Response:');
-      print(cityCurrentWeatherResponse.body);
-
       _currentCityWeatherData = currentCityWeatherDataFromJson(cityCurrentWeatherResponse.body);
-
-      print('JSON:');
-      print(_currentCityWeatherData);
     } else if (cityCurrentWeatherResponse.statusCode == 404) {
+      _currentCityWeatherData = null;
       ScaffoldMessenger.of(context).showSnackBar(new SnackBar(content: Text(Strings.cityNotFound)));
     } else {
+      _currentCityWeatherData = null;
       ScaffoldMessenger.of(context).showSnackBar(new SnackBar(content: Text(Strings.sorryAnErrorOccurred)));
+    }
+
+    if (_currentCityWeatherData != null) {
+      var dailyWeatherURL = Uri.parse('$OPEN_WEATHER_API_URL/forecast?lat=${_currentCityWeatherData!.coord.lat}&lon=${_currentCityWeatherData!.coord.lon}&appid=$OPEN_WEATHER_API_KEY&units=metric');
+      final dailyWeatherResponse = await http.get(dailyWeatherURL);
+
+      print('Daily Response:');
+      print(dailyWeatherResponse.body);
+
+      if (dailyWeatherResponse.statusCode == 200) {
+        _dailyWeatherData = dailyWeatherDataFromJson(dailyWeatherResponse.body);
+
+        print('Count: ${_dailyWeatherData!.list.length}');
+        for (var currentData in _dailyWeatherData!.list) {
+          print('Current: ');
+          print(currentData.dtTxt);
+        }
+      } else if (dailyWeatherResponse.statusCode == 404) {
+        _dailyWeatherData = null;
+        ScaffoldMessenger.of(context).showSnackBar(new SnackBar(content: Text(Strings.cityNotFound)));
+      } else {
+        _dailyWeatherData = null;
+        ScaffoldMessenger.of(context).showSnackBar(new SnackBar(content: Text(Strings.sorryAnErrorOccurred)));
+      }
+
     }
   }
 
