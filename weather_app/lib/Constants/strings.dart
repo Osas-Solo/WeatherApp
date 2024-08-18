134df4c33e5b4b63b58c6a3cf7d187646587311c
Osaremhen Ukpebor <osas.solo@outlook.com>
2024-08-18 15:29:09 +0100

    Added current city weather api

--- weather_app/lib/Constants/strings.dart
+++ weather_app/lib/Constants/strings.dart
@@ -1,3 +1,5 @@
 class Strings {
   static final enterCityName = 'Enter city name';
+  static final cityNotFound = 'City not found';
+  static final sorryAnErrorOccurred = 'Sorry, an error occurred';
 }
--- weather_app/lib/Screens/WeatherScreen.dart
+++ weather_app/lib/Screens/WeatherScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 
 import '../Constants/strings.dart';
 
@@ -13,6 +14,26 @@ class WeatherScreen extends StatefulWidget {
 class _WeatherScreenState extends State<WeatherScreen> {
   TextEditingController _cityController = new TextEditingController();
   final String OPEN_WEATHER_API_KEY = dotenv.env['OPEN_WEATHER_API_KEY']!;
+  final String OPEN_WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+  Future<void> _searchWeather() async {
+    final String cityName = _cityController.text;
+
+    var cityCurrentWeatherURL = Uri.parse('$OPEN_WEATHER_API_URL/weather?q=$cityName&appID=$OPEN_WEATHER_API_KEY');
+    final cityCurrentWeatherResponse = await http.get(cityCurrentWeatherURL);
+
+    print('Response status:');
+    print(cityCurrentWeatherResponse.body);
+    
+    if (cityCurrentWeatherResponse.statusCode == 200) {
+      print('Response:');
+      print(cityCurrentWeatherResponse.body);
+    } else if (cityCurrentWeatherResponse.statusCode == 404) {
+      ScaffoldMessenger.of(context).showSnackBar(new SnackBar(content: Text(Strings.cityNotFound)));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(new SnackBar(content: Text(Strings.sorryAnErrorOccurred)));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +46,9 @@ class _WeatherScreenState extends State<WeatherScreen> {
               controller: _cityController,
               hintText: Strings.enterCityName,
               leading: const Icon(Icons.search),
+              onSubmitted: (text) {
+                _searchWeather();
+              },
             ),
           ],
         ),
